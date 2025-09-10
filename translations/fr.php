c6763d792c9ebf6cbd3571c331658a68ee4bc561
Manu.L <manu@profil-web.fr>
2025-09-10 15:24:55 +0200

    add translation FR

---
+++ translations/fr.php
@@ -0,0 +1,11 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{pw_bandeau}prestashop>pw_bandeau_72102872afdf34b00f223f566992d691'] = 'PW Bandeau';
+$_MODULE['<{pw_bandeau}prestashop>pw_bandeau_d16858f80b823f3da98d4e7b9f7fe6e9'] = 'Affiche un bandeau text défilant';
+$_MODULE['<{pw_bandeau}prestashop>pw_bandeau_8dd2f915acf4ec98006d11c9a4b0945b'] = 'Configuration mise à jour';
+$_MODULE['<{pw_bandeau}prestashop>pw_bandeau_07619794bc6a56cb92d1a39885ebd7de'] = 'Configuration';
+$_MODULE['<{pw_bandeau}prestashop>pw_bandeau_9dffbf69ffba8bc38bc4e01abf4b1675'] = 'Texte défilant';
+$_MODULE['<{pw_bandeau}prestashop>pw_bandeau_40df3f02a2ceb144f5428fcea8fdbeb3'] = 'Saissez le texte que vous souhaitez voir défiler';
+$_MODULE['<{pw_bandeau}prestashop>pw_bandeau_c9cc8cce247e49bae79f15173ce97354'] = 'Enregistrer';
